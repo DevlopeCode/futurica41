b480b4e75c5494f8ec24b40d0fe179cf9dcc9c30
prabhat choudhary <prabhat@arithmate.com>
2024-11-26 13:16:22 +0400

    test

---
+++ src/components/Banner.js
@@ -0,0 +1,83 @@
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import headerImg from "../assets/img/header-img.svg";
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState('');
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
+  const toRotate = ["Full Stack Javascript Developer", "React Native Developer", "React js Developer", "Node Develope"];
+  const period = 2000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => { clearInterval(ticker) };
+  }, [text])
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta(prevDelta => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex(prevIndex => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(500);
+    } else {
+      setIndex(prevIndex => prevIndex + 1);
+    }
+  }
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="aligh-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                  <span className="tagline">Welcome to my Portfolio</span>
+                  <h1>{`Hi! I'm Prabhat`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Full Stack Javascript Devloper", "React Native Developer","React js Developer", "Node js Developer" ]'><span className="wrap">{text}</span></span></h1>
+                  <p>Fullstack JavaScript developer with 3+ years of experience dedicated to best practices such as code review, pair programming, standup, retro, tests. Sensitive to business needs and team dynamics. javascript, jQuery, HAML, Sass, css, MySQL, Postgres, aws, Heroku, Git, Jenkins, Hydra. Classroom experience with React and node js.
+<br/>
+                    I have created a responsive design which runs on Android, iOS and Web.
+                    I have completed many similar projects using JavaScript, UI/UX, Node, React-JS, and React Native in the recent past with 100% client satisfaction.
+                    <br/>
+                    Please message me anytime so we can have a detailed discussion.
+                    I am looking forward to your cooperation and support.</p>
+                  <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
+                </div>}
+            </TrackVisibility>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                  <img src={headerImg} alt="Header Img" />
+                </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  )
+}
---
+++ src/components/Footer.js
@@ -0,0 +1,29 @@
+import { Container, Row, Col } from "react-bootstrap";
+import { MailchimpForm } from "./MailchimpForm";
+import logo from "../assets/img/logo.png";
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/upwork.png';
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-items-center">
+          <MailchimpForm />
+          <Col size={12} sm={6}>
+            <img src={logo} alt="Logo" />
+          </Col>
+          <Col size={12} sm={6} className="text-center text-sm-end">
+            <div className="social-icon">
+            <a href="https://www.linkedin.com/in/prabhat-choudhary-641653150/"><img src={navIcon1} alt="" /></a>
+                <a href="https://www.facebook.com/profile.php?id=100048907991276"><img src={navIcon2} alt="" /></a>
+                <a href="https://www.upwork.com/freelancers/~019190b83a2e48cbfa?viewMode=1&s=1110580755107926016/"><img src={navIcon3} alt="" /></a>
+            </div>
+            <p>Copyright 2022. All Rights Reserved</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
